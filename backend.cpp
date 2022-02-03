920111d2ddd97849b44ea7cb3e04bad29c130e60
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2022-02-03 17:35:42 +0300

    Add files via upload

--- backend.cpp
+++ backend.cpp
@@ -3,11 +3,14 @@
 #include <string>
 #include <cmath>
 #include <sstream>
+#include "field.c"
+
 using namespace std;
 static float pi=3.1415926535897932384626;
 static float mu=1.25663706e-6;
 static float epsilon=8.8541878128e-12;
 static float pi4e0=4*pi*epsilon;
+
 
 void qp(string __){
     cout<<__<<endl;
@@ -16,10 +19,10 @@ void qp(float  __){
     cout<<__<<endl;
 }
 
-float norm(float* x, float* y)
+double norm(double* x, double* y)
 {
     uint8_t dim=sizeof(x)/3;
-    float __=0;
+    double __=0;
     for(uint8_t i;i<dim;i++)
     {
         __+=sqrt(pow(x[i],2)+pow(y[i],2));
@@ -29,9 +32,9 @@ float norm(float* x, float* y)
 
 }
 
-float dot_product(float* A, float* B){
+double dot_product(float* A, float* B){
     uint8_t vl=sizeof(A)/4;
-    float val=0;
+    double val=0;
     for(uint8_t i=0;i<vl;){
         val+=A[0]*B[0];
     }
@@ -40,18 +43,7 @@ float dot_product(float* A, float* B){
 
 //field function begins
 
-float fieldX(float* P)
-{
-return 1;
-}
-float fieldY(float* P)
-{
-return 1;
-}
-float fieldZ(float* P)
-{
-return 1;
-}
+
 //field function begins
 
 int main()
@@ -65,8 +57,9 @@ int main()
        n_of_points++;
 
     }
-    float points[n_of_points][3];
-    float normals[n_of_points][3];
+    double points[n_of_points][3];
+    double normals[n_of_points][3];
+   
     ioc.close();
     fstream iop;
    
@@ -128,17 +121,20 @@ int main()
     }
     ion.close();
 //field data:
-float r_val;
+double r_val;
 
 for (unsigned long i=0; i<n_of_points;i++)
 {
-float field_vector[3];
-field_vector[0]=fieldX(points[0]);
-field_vector[1]=fieldY(points[1]);
-field_vector[2]=fieldZ(points[2]);
-r_val+=fieldX(points[i])*normals[i][0];
-r_val+=fieldY(points[i])*normals[i][0];
-r_val+=fieldZ(points[i])*normals[i][0];
+
+double __=fieldX(points[i][0],points[i][1],points[i][2])*normals[i][0]+fieldY(points[i][0],points[i][1],points[i][2])*normals[i][1]+fieldZ(points[i][0],points[i][1],points[i][2])*normals[i][2];
+
+if (isnan(__)==false)
+{
+r_val+=__;
+
+
+}
+
 
 
 }
