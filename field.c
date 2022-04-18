42e1df86ed2ae4c46bc623d851faf06b46753a6c
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2022-04-18 18:29:40 +0300

    Update field.c

--- field.c
+++ field.c
@@ -1,2 +1,2 @@
 #include<cmath> 
-float fieldX(float x,float y, float z){return x;} float fieldY(float x,float y, float z){return y;}float fieldZ(float x,float y, float z){return x;} 
+float fieldX(float x,float y, float z){return x;} float fieldY(float x,float y, float z){return y;}float fieldZ(float x,float y, float z){return z;} 
